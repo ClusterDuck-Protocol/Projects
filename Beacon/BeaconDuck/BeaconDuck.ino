2675d8de5cb577a8cd6e7d04f9863d5d25f80fbe
Taraqur <tqrahman8@gmail.com>
2024-10-14 19:46:53 -0400

    updated calpoly firmware
    Signed-off-by: Taraqur <tqrahman8@gmail.com>

---
+++ Beacon/BeaconDuck/BeaconDuck.ino
@@ -0,0 +1,119 @@
+/**
+ * @file BeaconDuck.ino
+ * @brief A MamaDuck that sends the message "QUACK QUACK QUACK QUACK" repeatedly.
+ */
+
+std::string deviceId("QUACKER1"); // DuckID - NEEDS to be 8 characters
+const int INTERVAL_MS = 30000; // for sending the counter message
+
+#include <string>
+#include <arduino-timer.h>
+#include <CDP.h>
+#include "FastLED.h"
+
+// Setup for W2812 (LED)
+#define LED_TYPE WS2812
+#define DATA_PIN 4
+#define NUM_LEDS 1
+#define COLOR_ORDER GRB
+#define BRIGHTNESS  128
+#include <pixeltypes.h>
+CRGB leds[NUM_LEDS];
+
+#ifdef SERIAL_PORT_USBVIRTUAL
+#define Serial SERIAL_PORT_USBVIRTUAL
+#endif
+
+bool sendData(std::vector<byte> message, topics value);
+bool runSensor(void *);
+bool runGPS(void *);
+
+// create a built-in mama duck
+MamaDuck duck;
+
+// create a timer with default settings
+auto timer = timer_create_default();
+
+int counter = 1;
+bool setupOK = false;
+
+void setup() {
+  // LED Intial
+  FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalSMD5050 );
+  FastLED.setBrightness(  BRIGHTNESS );
+  leds[0] = CRGB::Red;
+  FastLED.show();
+
+  //The Device ID must be exactly 8 bytes otherwise it will get rejected
+  std::vector<byte> devId;
+  devId.insert(devId.end(), deviceId.begin(), deviceId.end());
+  
+  if (duck.setupWithDefaults(devId) != DUCK_ERR_NONE) {
+    Serial.println("[MAMA] Failed to setup MamaDuck");
+    return;
+  }
+
+  // Initialize the timer for telemetry
+  timer.every(INTERVAL_MS, runSensor);
+
+  // LED Complete
+  leds[0] = CRGB::Gold;
+  FastLED.show();
+
+  setupOK = true;
+
+  Serial.println("[MAMA] Setup OK!");
+}
+
+std::vector<byte> stringToByteVector(const String& str) {
+    std::vector<byte> byteVec;
+    byteVec.reserve(str.length());
+
+    for (unsigned int i = 0; i < str.length(); ++i) {
+        byteVec.push_back(static_cast<byte>(str[i]));
+    }
+
+    return byteVec;
+}
+
+void loop() {
+  if (!setupOK) {
+    return; 
+  }
+  timer.tick();
+  // Use the default run(). The Mama duck is designed to also forward data it receives
+  // from other ducks, across the network. It has a basic routing mechanism built-in
+  // to prevent messages from hoping endlessly.
+  duck.run();
+}
+
+bool runSensor(void *) {
+  bool result;
+
+  string statusMessage = String("QUACK QUACK QUACK QUACK");
+  Serial.print("[MAMA] status data: ");
+  Serial.println(statusMessage);
+
+  result = sendData(stringToByteVector(healthMessage), status);
+  if (result) {
+     Serial.println("[MAMA] runSensor ok.");
+  } else {
+     Serial.println("[MAMA] runSensor failed.");
+  }
+
+  return result;
+}
+
+bool sendData(std::vector<byte> message, topics value) {
+  bool sentOk = false;
+  
+  int err = duck.sendData(value, message);
+  if (err == DUCK_ERR_NONE) {
+     counter++;
+     sentOk = true;
+  }
+  if (!sentOk) {
+    Serial.println("[MAMA] Failed to send data. error = " + String(err));
+  }
+  return sentOk;
+}
